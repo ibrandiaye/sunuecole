bc6299e879e0e49136f3258de860701aa0b26a3f
ibrandiaye <ibra789ndiaye@gmail.com>
2026-09-15 10:57:12 +0000

    ee

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 namespace App\Services;
 
 use App\Models\NotificationPush;
---
+++ rm_bom.php
@@ -0,0 +1,6 @@
+﻿<?php
+$f='app/Services/NotificationService.php';
+$c=file_get_contents($f);
+if(substr($c,0,3)==="\xEF\xBB\xBF") {
+    file_put_contents($f, substr($c,3));
+}
